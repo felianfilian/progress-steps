e2f64d2e3a6b5dfc8a19aa03cf030892324edfa8
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-10-17 10:39:44 +0200

    script selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+let progress = document.getElementById("steps");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let circles = document.querySelectorAll(".circle");
