e21a7a4986d2e32775fa0a5b715e650faa7eb627
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-10-18 20:19:29 +0200

    progress logik

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-let progress = document.getElementById("steps");
+let progress = document.getElementById("progress");
 let prev = document.getElementById("prev");
 let next = document.getElementById("next");
 let circles = document.querySelectorAll(".circle");
+
+let currentAcitve = 1;
+
+prev.addEventListener("click", () => {
+  currentAcitve--;
+  if (currentAcitve < 1) {
+    currentAcitve = 1;
+  }
+  update();
+});
+
+next.addEventListener("click", () => {
+  currentAcitve++;
+  if (currentAcitve >= circles.length) {
+    currentAcitve = circles.length;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentAcitve) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentAcitve <= 1) {
+    prev.disabled = true;
+  } else if (currentAcitve >= circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
